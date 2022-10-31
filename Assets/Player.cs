fa425d552662b82d1c315cd1dcc2c4794952b524
mohazahid <adyanzahid786@gmail.com>
2022-10-31 01:04:35 -0700

    moving farquad

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        transform.position = pos;
     }
 }
---
+++ Assets/PlayerCam.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public Transform player;
+    public BoxCollider2D mapBounds;
+
+    private float xMin, xMax, yMin, yMax;
+    private float camY,camX;
+    private float camOrthsize;
+    private float cameraRatio;
+    private Camera mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+          xMin = mapBounds.bounds.min.x;
+        xMax = mapBounds.bounds.max.x;
+        yMin = mapBounds.bounds.min.y;
+        yMax = mapBounds.bounds.max.y;
+        mainCam = GetComponent<Camera>();
+        camOrthsize = mainCam.orthographicSize;
+        cameraRatio = (xMax + camOrthsize) / 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update() {        
+         camY = Mathf.Clamp(player.position.y, yMin + camOrthsize, yMax - camOrthsize);
+        camX = Mathf.Clamp(player.position.x, xMin + cameraRatio, xMax - cameraRatio);
+        this.transform.position = new Vector3(camX, camY, this.transform.position.z);
+    }
+}
