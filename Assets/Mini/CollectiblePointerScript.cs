581f345c57a1c8db7c063c8ccc61b69a20de36e3
mohazahid <adyanzahid786@gmail.com>
2022-11-21 04:41:21 -0800

    tutorial updated

--- Assets/Mini/CollectiblePointerScript.cs
+++ Assets/Mini/CollectiblePointerScript.cs
@@ -42,7 +42,6 @@ public class CollectiblePointerScript : MonoBehaviour
             else
             {
                 SetArrowActive(true);
-                Debug.Log("dir" + dir.magnitude); 
             }
             var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
