bb2cff915f13220528e55afcb8cebd4b2e483f7f
mohazahid <adyanzahid786@gmail.com>
2022-11-24 03:36:21 -0800

    player

---
+++ Assets/potatoAudioScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potatoAudioScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int SelfCounter = 0;
+    private AudioSource PotatoSound;
+    void Start()
+    {
+        AudioSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.potatoCount != SelfCounter)
+        {
+            SelfCounter = Player.potatoCount;
+            playerSound.Play();
+        }
+    }
+}
