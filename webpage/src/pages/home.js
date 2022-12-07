c48f027679c1125ef8337dca3315d7d395fb6879
akumar282 <abhi.kumar7567@gmail.com>
2022-12-07 01:40:52 -0800

    water on me like a sauna

---
+++ webpage/src/pages/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function HomePage() {
+  return ( 
+    <div>
+      <li> 
+        <Link to='/'></Link>
+      </li>
+      <li> 
+        <Link to='/'></Link>
+      </li>
+      <li> 
+        <Link to='/'></Link>
+      </li>
+      <li> 
+        <Link to='/'></Link>
+      </li>
+    </div> 
+  )
+}
