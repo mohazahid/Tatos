9015fd5034ec8c69024c1b00c0cc79af238580dc
mohazahid <adyanzahid786@gmail.com>
2022-12-14 17:21:04 -0800

    working link

--- webpage/src/pages/game.js
+++ webpage/src/pages/game.js
@@ -1,12 +1,19 @@
+import LogoImg from "../Images/MovieLogoTatos.png";
+function LoadGame() {
+    window.open("https://mohazahid.github.io/Tatos/FinalBuild/index.html");
+}
 export default function Game() {
     return (
     <div className = "GamePage">
-    <h1> Game </h1>
-    <div className = "TextDiv">
-        <box className = "TextBox">
-            <h2> Game img </h2>
-        </box>
-    </div>
+        <div className = "TextDiv">
+            <box className = "TextBox">
+                <img src={LogoImg} width={500} height={650} alt="passedImg"/>
+                
+            </box>
+            <button className = "LinkButton" onClick = {LoadGame}>
+                Play The Game
+            </button>
+        </div>
     </div>
     )
 }
